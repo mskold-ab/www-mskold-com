65729888516f0faaafdc584dfc8c118ac6b9a2ba
Mattias SKöld <mattias.skold@mskold.com>
2026-06-17 10:08:34 +0200

    Update gh_alerts.rb

--- _plugins/gh_alerts.rb
+++ _plugins/gh_alerts.rb
@@ -1,13 +1,22 @@
-Jekyll::Hooks.register :documents, :pre_render do |doc|
-  doc.content.gsub!(
-    /^> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]\n((?:>.*\n?)*)/m
-  ) do
-    type = $1.downcase
-    body = $2.gsub(/^>\s?/, '')
+ALERT_REGEX = /^> \[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]\r?\n((?:(?:>\s?.*)\r?\n?)*)/m
+
+def transform_github_alerts(content)
+  content.gsub(ALERT_REGEX) do
+    type = Regexp.last_match(1).downcase
+    body = Regexp.last_match(2).gsub(/^>\s?/, '').strip
+
     <<~HTML
     <div class="gh-alert gh-alert-#{type}">
     #{body}
     </div>
     HTML
   end
+end
+
+Jekyll::Hooks.register :pages, :pre_render do |page|
+  page.content = transform_github_alerts(page.content)
+end
+
+Jekyll::Hooks.register :documents, :pre_render do |doc|
+  doc.content = transform_github_alerts(doc.content)
 end
